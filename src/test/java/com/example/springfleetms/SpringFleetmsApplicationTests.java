0863efa62311207b2e3951d05418bc9ba962d0b0
shevkosasha <shevkosasha@gmail.com>
2024-02-15 00:33:42 +0300

    fleet ms first commit

---
+++ src/test/java/com/example/springfleetms/SpringFleetmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfleetms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFleetmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
